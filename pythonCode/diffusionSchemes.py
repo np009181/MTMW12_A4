5e0eec414ab3df8ed8971a8e2668eb5ac2ae6763
np009181 <32457098+np009181@users.noreply.github.com>
2017-10-18 14:07:23 +0100

    Update diffusionSchemes.py
    Adding to the original code

--- pythonCode/diffusionSchemes.py
+++ pythonCode/diffusionSchemes.py
@@ -6,3 +6,17 @@ import numpy as np
 # The linear algebra package for BTCS (for solving the matrix equation)
 import scipy.linalg as la
 
+def FTCS(phiOld, d, nt):
+    nx = len(phiOld)
+    
+    #Create a new time-step array for phi#
+    phi = phiOld.copy()
+    
+    #FTCS for all time steps#
+    for it in xrange(int(nt)):
+        phi[0] = ???
+        
+        
+        
+    for i in range (1, nx - 1):
+        phi[i] = ???
